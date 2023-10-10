b3e4906ad2cd02c30ea21e6fec01aba25c10cf5b
Nuraina17 <nuraina170204@gmail.com>
2023-10-10 13:59:38 +0700

    ini adalah codingan aina

---
+++ DAY2.java
@@ -0,0 +1,29 @@
+package insyaallah;
+
+public class DAY2 {
+   public static void main(String[] args) {
+        byte byteVar = 120;
+        short shortVar= 32767;
+        int intVar = 2134567898;
+        long longVar = 9223372036854775805L;
+        float floatVar =678.34f;
+        double doubleVar =675.954321;
+        char charVar = 'B';
+        boolean booleanVar =true;
+        
+        System.out.println("byteVar   :"+byteVar);
+        System.out.println("shortVar  :"+shortVar);
+        System.out.println("intVar    :"+intVar);
+        System.out.println("longVar   :"+longVar);
+        System.out.println("floatVar  :"+floatVar);
+        System.out.println("doubleVar :"+doubleVar);
+        System.out.println("charVar   :"+charVar);
+        System.out.println("booleanVar:"+booleanVar);
+        
+    }
+}
+
+    
+
+
+
